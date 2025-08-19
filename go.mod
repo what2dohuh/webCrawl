43c205264dca7362c5de866d7367506f39429212
Deepjyoti Sharma <deepsharm17@gmail.com>
2025-08-19 14:13:23 +0530

    MongoDB connection..

--- go.mod
+++ go.mod
@@ -2,4 +2,20 @@ module webcrawl.com/m
 
 go 1.24.4
 
-require golang.org/x/net v0.42.0
+require (
+	github.com/joho/godotenv v1.5.1
+	go.mongodb.org/mongo-driver/v2 v2.3.0
+	golang.org/x/net v0.42.0
+)
+
+require (
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 	"golang.org/x/net/html"
 	"strings"
 	"bytes"
+	"os"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
+	"context"
+	"github.com/joho/godotenv"
 )
+
 
 type Queue struct {
 	elements []string
@@ -223,7 +231,39 @@ func NormalizeUrl(rawURL string) string{
 	u.RawQuery = ""
 	return u.String()
 }
+
+
+
+func connectMongo(){
+	var uri string
+	err := godotenv.Load()
+	if err!=nil{
+		log.Fatal("Error while etching env")
+	}
+
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	client,err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+}
 func main() {
+	connectMongo()
 	q := &Queue{
 		elements: make([]string, 0),
 	}
